8141b37bc03000b4c948f1557dab5da47a7e4b07
nengg <trongnang1682000@gmail.com>
2022-04-20 00:56:15 +0700

    khong can active-page

--- form/pagination.php
+++ form/pagination.php
@@ -15,22 +15,32 @@
         .wrapper {
             height: 100vh;
             display: flex;
+            flex-direction: column;
             justify-content: center;
-            align-items: center
+            align-items: center;
+        }
+
+        .pagination {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 10px auto;
+            text-align: center;
+            flex-wrap: wrap
         }
 
         .page-link {
             position: relative;
             display: block;
-            color: #673AB7 !important;
+            color: #673AB7;
             text-decoration: none;
             background-color: #fff;
-            border: 1px solid #673AB7 !important
+            border: 1px solid #673AB7
         }
 
         .page-link:hover {
             z-index: 2;
-            color: #fff !important;
+            color: #fff;
             background-color: #673ab7;
             border-color: #024dbc
         }
@@ -47,16 +57,22 @@
     <div class="wrapper">
         <nav aria-label="Page navigation example">
             <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">4</a></li>
-                <li class="page-item"><a class="page-link" href="#">5</a></li>
-                <li class="page-item"><a class="page-link" href="#">6</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=1">1</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=2">2</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=3">3</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=4">4</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=5">5</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=6">6</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=7">7</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=8">8</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=9">9</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php?page=10">10</a></li>
+                <li class="page-item"><a class="page-link" href="pagination.php">Next</a></li>
             </ul>
         </nav>
+        <?php $page = (isset($_GET["page"]) == true) ? $_GET["page"] : "0" ?>
+        <p class="result">Bạn đang ở trang thứ <?php echo $page; ?></p>
     </div>
 </body>
 
