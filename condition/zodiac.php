31a257f828d4d479e3b6c88cb452f4da3979ee4c
nengg <trongnang1682000@gmail.com>
2022-04-23 22:05:11 +0700

    khong hieu sao sua css nhung local host khong thay doi gi

---
+++ condition/zodiac.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_zod.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="content">
+        <h1>WHAT IS YOUR ZODIAC SIGN?</h1>
+        <form action="#" method="POST" name="main-form">
+            <div class="row">
+                <span>Day</span>
+                <input type="text" name="day" value="<?php ?>">
+            </div>
+            <div class="row">
+                <span>Month</span>
+                <input type="text" name="month" value="<?php ?>">
+            </div>
+            <div class="row">
+                <input type="submit" name="submit" value="See Result">
+            </div>
+            <div class="row">
+                <p>
+                </p>
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
